984dab93d0312369c2a6cf6b5e3c8afab69368d9
Kevin <2.kevincarroll@gmail.com>
2021-08-04 15:17:09 -0700

    Starting Unit Test Module.

--- tests/ftp.tests.py
+++ tests/ftp.tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
@@ -16,8 +17,6 @@ class TestStringMethods(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
-    def test_shouldfail(self):
-        self.assertEqual('true', 'false')
 
 if __name__ == '__main__':
     unittest.main()
