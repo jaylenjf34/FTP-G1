a4cbc2d57dbc1be7c0bac46a0bf2e3798e5fae9f
Nicki Wahlers <61523696+nickiwahlers111@users.noreply.github.com>
2021-08-10 18:55:02 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -11,10 +11,8 @@ from .put import *
 from .put_mutiple import *
 from .get import *
 from .get_multiple import *
-<<<<<<< HEAD
-=======
 from .chmod import *
 from .lls import *
 from .lpwd import *
 from .lrename import *
->>>>>>> 3422c0a0859005d7c2ba71c48af3fc33d99ac62e
+
